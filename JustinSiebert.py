c1a1b725bed0c3624e8673eaf154445d4875696a
Justin Siebert <just.sieb@gmail.com>
2016-07-14 13:48:03 -0400

    Moved future feature to different branch

--- JustinSiebert.py
+++ JustinSiebert.py
@@ -3,13 +3,6 @@ from flask import Flask, url_for, send_from_directory, render_template, abort, s
 
 app = Flask(__name__, static_url_path='/static')
 
-app.config.update(dict(
-    DATABASE=os.path.join(app.root_path, 'JustinSiebert.db'),
-    SECRET_KEY='BobNewhart',
-    USERNAME='Just.Sieb',
-    PASSWORD='TempPass'
-))
-
 @app.route('/static/<path:path>')
 def send_static(path):
     return send_from_directory('static', path)
@@ -20,89 +13,14 @@ def send_static_css(path):
     return send_from_directory('static/css', path)
 
 
-@app.route('/static/fonts/<path:path>')
-def send_static_fonts(path):
-    return send_from_directory('static/fonts', path)
-
-
-@app.route('/static/html/<path:path>')
-def send_static_html(path):
-    return send_from_directory('static/html', path)
-
-
 @app.route('/static/js/<path:path>')
 def send_static_js(path):
     return send_from_directory('static/js', path)
     
 
-@app.route('/static/pictures/<path:path>')
-def send_static_pictures(path):
-    return send_from_directory('static/pictures', path)
-
-
 @app.route('/')
 def root():
     return render_template('main.html')
-
-
-@app.route('/about/')
-def about():
-    return render_template('about.html')
-
-
-@app.route('/contact/')
-def contact():
-    return render_template('contact.html')
-
-
-@app.route('/LEGO/')
-def lego():
-    return render_template('lego.html')
-
-
-@app.route('/LEGO/<build>/')
-def build(build):
-    return "Player %s" % build
-
-
-@app.route('/programming/')
-def programming():
-    return render_template('programming.html')
-
-
-@app.route('/programming/<program>')
-def program(program_name):
-    return "Program %s" % program_name
-
-       
-@app.route('/admin/')
-def admin():
-    if not session.get('logged_in'):
-        print("Mission Abort")
-        return redirect(url_for('login'))
-    return render_template('admin.html')
-  
-  
-@app.route('/login/', methods=['get', 'post'])
-def login():
-    error = None
-    if request.method == 'POST':
-        if request.form['username'] != app.config['USERNAME']:
-            error = 'Invalid Username'
-        elif request.form['password'] != app.config['PASSWORD']:
-            error = 'Invalid Password'
-        else:
-            session['logged_in'] = True
-            flash('Your quantum state was changed to being logged in.') 
-            return redirect(url_for('admin'))
-    return render_template('login.html')
-  
-  
-@app.route('/logout')
-def logout():
-    session.pop('logged_in', None)
-    flash('Signed Out')
-    return redirect(url_for('root'))
   
   
 @app.errorhandler(401)
